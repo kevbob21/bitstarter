fb814bc62d22b31e3207661a0ff50443712f7581
Ubuntu <ubuntu@ip-172-31-39-181.us-west-2.compute.internal>
2013-07-11 04:34:21 +0000

    Update web.js readFilSync5

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = fs.readFileSync(index.html);
+var buffer2 = buf.toString(buffer);
 
 app.get('/', function(request, response) {
-   response.send(buf.toString(buffer);
+   response.send(buffer2);
    //response.send('Hello World3!');
 });
 
