977fd3d91d5b23f5ed6d822b8d21a365df21b46c
kevbob21 <kevbob21@gmail.com>
2013-07-21 05:10:15 +0000

    Edited product title in index.html

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html"
 
 var app = express.createServer(express.logger());
 
@@ -7,12 +8,12 @@ var app = express.createServer(express.logger());
 //var buffer2 = buf.toString(buffer);
 
 app.get('/', function(request, response) {
-    response.end(fs.readFileSync(__dirname + '/index.html'));
-    //response.send(buffer2);
-    //response.send('Hello World3!');
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+    //response.end(fs.readFileSync(__dirname + '/index.html'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
